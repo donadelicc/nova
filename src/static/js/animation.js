ba6ffd3736d14261c636abc267bee4bd06f734a0
donadelicc <preb1anders1@gmail.com>
2024-04-20 15:41:27 +0200

    small upd

--- src/static/js/animation.js
+++ src/static/js/animation.js
@@ -1,11 +1,15 @@
 const activationWaiting = document.querySelector('.start');
+const wakeWord = document.querySelector('.vekkeord').parentNode;
 const questionWaiting = document.querySelector('.image-ear').parentNode;
+const askQuestion = document.querySelector('.spørsmål').parentNode;
 const processing = document.querySelector('.image-process').parentNode;
 const speaking = document.querySelector('.image-speak').parentNode;
 
 function hideAllStates() {
     activationWaiting.style.opacity = '0';
+    wakeWord.style.opacity = '0';
     questionWaiting.style.opacity = '0';
+    askQuestion.style.opacity = '0';
     processing.style.opacity = '0';
     speaking.style.opacity = '0';
 }
@@ -16,9 +20,11 @@ export function updateUI(state) {
     switch(state) {
         case 'activate':
             activationWaiting.style.opacity = '1';
+            wakeWord.style.opacity = '1';
             break;
         case 'listening':
             questionWaiting.style.opacity = '1';
+            askQuestion.style.opacity = '1';
             break;
         case 'processing':
             processing.style.opacity = '1';
